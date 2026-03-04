4d1ec11ffe7da30b78f4e8e5c525d55b7293ca66
loihnt05 <23520869@gm.uit.edu.vn>
2026-03-04 22:11:19 +0700

    connect with api dbcontext

--- be/BlogPlatform.API/Program.cs
+++ be/BlogPlatform.API/Program.cs
@@ -1,9 +1,13 @@
+using BlogPlatform.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BlogDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
