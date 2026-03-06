431194ffaa27509d48bed5ed8966280dffe3d5f8
loihnt05 <23520869@gm.uit.edu.vn>
2026-03-06 15:38:41 +0700

    setup dbcontext

--- be/BlogPlatform.Infrastructure/Persistence/BlogDbContext.cs
+++ be/BlogPlatform.Infrastructure/Persistence/BlogDbContext.cs
@@ -9,6 +9,11 @@ public class BlogDbContext : DbContext
     {
     }
     public DbSet<Post> Posts => Set<Post>();
+    public DbSet<Comment> Comments => Set<Comment>();
+    public DbSet<User> Authors => Set<User>();
+    public DbSet<Tag> Tags => Set<Tag>();
+    public DbSet<PostTag> PostTags => Set<PostTag>();
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
