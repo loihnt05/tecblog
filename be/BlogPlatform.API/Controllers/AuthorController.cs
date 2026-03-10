893346eb54a0525d8ca5dd6dfccd78ecbe1bfb0a
loihnt05 <23520869@gm.uit.edu.vn>
2026-03-10 10:48:24 +0700

    author controller

---
+++ be/BlogPlatform.API/Controllers/AuthorController.cs
@@ -0,0 +1,35 @@
+using BlogPlatform.Application.Authors.Commands.CreateAuthor;
+using BlogPlatform.Application.Authors.Queries.GetAuthorById;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace BlogPlatform.API.Controllers;
+
+[ApiController]
+[Route("api/authors")]
+public class AuthorController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    public AuthorController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateAuthorCommand command)
+    {
+        var id = await _mediator.Send(command);
+
+        return CreatedAtAction(nameof(GetById), new { id }, id);
+    }
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var author = await _mediator.Send(new GetAuthorByIdQuery(id));
+        if (author == null)
+            return NotFound();
+            
+        return Ok(author);
+    }
+    
+}
